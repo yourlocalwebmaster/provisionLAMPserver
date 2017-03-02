cab1031ed007a92d80a713c78083a3c05497a324
Grant Kimball <gkimball@na-sdo-mws2762.na.huntsworthhealth.net>
2017-03-02 10:40:32 -0800

    added sudo apt-get install php7.0-zip

--- provision.sh
+++ provision.sh
@@ -24,7 +24,7 @@ echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 chown www-data:www-data /var/www/html/info.php
 
 #install optional dependencies
-apt-get -y install php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache  php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext php-mbstring php-mcrypt php-gd
+apt-get -y install sudo apt-get install php7.0-zip php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache  php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext php-mbstring php-mcrypt php-gd
 
 apt-get install npm nodejs
 
